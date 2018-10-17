2f1f0efa96304e56e892a88a4d622ebcefebeebb
peterlemon <krom@gbadev.org>
2018-10-17 05:25:34 +0100

    Fix compilation on latest n64chain

--- src/cartrom.c
+++ src/cartrom.c
@@ -3,5 +3,5 @@
 const unsigned char __cartrom[1] = { // Place "rom.gb" Into This Array
   0
 };
-const unsigned __cartrom_size = 1; // Set "rom.gb" Array Size
+const unsigned __cartrom_size = sizeof(__cartrom); // Set "rom.gb" Array Size
 const unsigned char _binary_filesystem_bin_start = 0;
